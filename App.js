5bc45b281f717bc45f301ce9162463ef8072d71e
abk-gami <yahabubakar01@gmail.com>
2024-01-09 15:36:56 +0100

    6

--- App.js
+++ App.js
@@ -3,8 +3,28 @@ import { StyleSheet, Text, View, Button } from 'react-native';
 import React, {useState} from 'react';
 
 export default function App() {
+ 
+  const [joke, setJoke] = useState('')
 
-  const [joke, setJoke] = ("")
+  const getJoke = async () => {
+    try {
+      // let response = await fetch("http://api.icndb.com/jokes/?escape=html&limitTo=[nerdy]")
+      // if (!response.ok) throw new Error(response.statusText);
+      // let data = await response.json();
+      // console.log(data.value.joke)
+      // setJoke(data.value.joke)
+      // } catch (error) {
+      //   alert(`Algo deu errado: ${error}`)
+    // }
+
+      let response = await fetch('https://official-joke-api.appspot.com/random_joke');
+      let data = await response.json();
+      setJoke( data.setup + '\n' + data.punchline);
+  } catch (error) {
+    // alert(`Erro ao buscar o chiste: ${error}`);
+    console.error(error)
+  }
+}
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
