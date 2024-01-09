bcf7aee284dfca5a25a5d27ff434f122328f1934
abk-gami <yahabubakar01@gmail.com>
2024-01-09 18:02:57 +0100

    e

--- App.js
+++ App.js
@@ -6,16 +6,17 @@ export default function App() {
  
   const [joke, setJoke] = useState('')
   const [punchline, setPunchline] = useState('')
+  const [loading, setLoading] = useState('Get Joke')
 
   const getJoke = async () => {
     try {
-    
-
+      setLoading('Generating Joke...')
       let response = await fetch('https://official-joke-api.appspot.com/random_joke');
       let data = await response.json();
       // console.log(data)
       setJoke( data.setup );
       setPunchline(data.punchline)
+      setLoading('Get Joke')
   } catch (error) {
     alert('There is an error:' + error);
     // console.error(error)
@@ -25,7 +26,7 @@ export default function App() {
     <View style={styles.container}>
       <Text>{joke}</Text>
       <Text>{punchline}</Text>
-      <Button title='Get Joke' onPress={getJoke}/>
+      <Button title={loading} onPress={getJoke}/>
     </View>
   );
 }
