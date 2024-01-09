9b4884fbe229e4eb83138d6c9d13234c15bea230
abk-gami <yahabubakar01@gmail.com>
2024-01-09 14:55:11 +0100

    c

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import { StyleSheet, Text, View, Button } from 'react-native';
 import React, {useState} from 'react';
 
 export default function App() {
+
+  const [joke, setJoke] = ("")
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
