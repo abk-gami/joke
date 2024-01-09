7115a1f2a54bf5ec078c5dac7b6b678bf5c366c2
abk-gami <yahabubakar01@gmail.com>
2024-01-09 14:53:28 +0100

    q

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import React, {useState} from 'react';
 
 export default function App() {
   return (
