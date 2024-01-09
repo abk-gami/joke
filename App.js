22cba1b5ae3d10d9342301a787d7867164dc3d7b
abk-gami <yahabubakar01@gmail.com>
2024-01-09 15:38:03 +0100

    1

--- App.js
+++ App.js
@@ -27,7 +27,7 @@ export default function App() {
 }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Get Joke</Text>
     </View>
   );
 }
